b4611afd03a46329007bd71fcf22f0a4b28e510e
Archer Li <archer.li@shopeemobile.com>
2018-05-18 11:30:38 +0800

    test

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from Cocoa import Cocoa
+from Cocoa import Cocoa, request
 
 
 app = Cocoa()
 
+
 @app.route('/welcome/<username>')
 def welcome(username):
-    ''' just welcome '''
+    """ just welcome """
+    print request.headers
     return '<h1>Hello {}, this is Cocoa web framework</h1>'.format(username)
 
 
