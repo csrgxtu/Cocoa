8b2efc75bb5c9ce1ff1acd654dc08aaf022dede3
archer.li <archer.li@shopee.com>
2019-06-25 16:20:04 +0800

    content-type support

--- Cocoa.py
+++ Cocoa.py
@@ -2,6 +2,8 @@ from wsgiref.simple_server import make_server
 from werkzeug.wrappers import Request as RequestBase, Response as ResponseBase
 from werkzeug.local import LocalStack, LocalProxy
 import re
+import logging as log
+import json
 
 
 _request_context_stack = LocalStack()
@@ -67,8 +69,13 @@ class Cocoa(object):
             route_match = self.get_route_match(path)
             if route_match:
                 kwargs, view_function = route_match
-                start_response('200 OK', [('Content-Type', 'text/html')])
-                return view_function(**kwargs)
+                ret = view_function(**kwargs)
+                body, content_type = self.__set_content_type_and_body(
+                    view_ret=ret,
+                    resource_extension=self.__get_request_resource_extension(path)
+                )
+                start_response('200 OK', [('Content-Type', content_type)])
+                return body
             else:
                 start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
                 return 'Route "{}" has not been registered'.format(path)
@@ -80,5 +87,33 @@ class Cocoa(object):
     def run(self, host, port):
         """ start a embed wsgi compatible http web server """
         httpd = make_server(host, port, self.application)
-        print 'Serving at {}:{}'.format(host, port)
+        log.info('Cocoa start at {}:{}'.format(host, port))
         httpd.serve_forever()
+
+    def __get_request_resource_extension(self, path_str):
+        """ get extension from path_str """
+        import os
+        _, file_extension = os.path.splitext(path_str)
+        return file_extension
+
+    def __set_content_type_and_body(self, view_ret, resource_extension):
+        """ """
+        body = view_ret
+        if resource_extension == '.css':
+            content_type = 'text/css'
+        elif resource_extension == '.js':
+            content_type = 'application/javascript'
+        elif resource_extension in ['.html', '.hml']:
+            content_type = 'text/html'
+        elif resource_extension in ['.jpeg', '.jpg']:
+            content_type = 'image/jpeg'
+        elif resource_extension == '.png':
+            content_type = 'image/png'
+        else:
+            if type(view_ret) is dict:
+                content_type = 'application/json'
+                body = json.dumps(view_ret, indent=2)
+            else:
+                content_type = 'text/html'
+
+        return body, content_type
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Cocoa import Cocoa, request
-
+import json
 
 app = Cocoa()
 
@@ -7,8 +7,21 @@ app = Cocoa()
 @app.route('/welcome/<username>')
 def welcome(username):
     """ just welcome """
+    print 'welcome: ', type(request.headers)
     print request.headers
+    print request.headers.get('Content-Type')
     return '<h1>Hello {}, this is Cocoa web framework</h1>'.format(username)
+
+@app.route('/json')
+def json_ret():
+    return {
+        'name': 'Cocoa'
+    }
+
+@app.route('/static/<file_name>')
+def static_file(file_name):
+    with open('/tmp/{}'.format(file_name), 'r') as file_handler:
+        return file_handler.read()
 
 
 app.run(host='0.0.0.0', port=8000)
